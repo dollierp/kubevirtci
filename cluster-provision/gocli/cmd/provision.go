d6e256d17ee125598568252f5cd0e0adda30ee90
Denis Ollier <dollierp@redhat.com>
2026-07-26 21:25:10 +0200

    chore(cluster-provision): remove unused manifests
    Signed-off-by: Denis Ollier <dollierp@redhat.com>

--- cluster-provision/gocli/cmd/provision.go
+++ cluster-provision/gocli/cmd/provision.go
@@ -287,10 +287,6 @@ func provisionCluster(cmd *cobra.Command, args []string) (retErr error) {
 			return err
 		}
 
-		err = _cmd(cli, nodeContainer(prefix, nodeName), fmt.Sprintf("ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i vagrant.key %s@192.168.66.101 'mkdir -p /tmp/ceph /tmp/cnao /tmp/nfs-csi /tmp/nodeports /tmp/prometheus /tmp/whereabouts /tmp/kwok'", sshUser), "Create required manifest directories before copy")
-		if err != nil {
-			return err
-		}
 		// Copy manifests to the VM
 		err = _cmd(cli, nodeContainer(prefix, nodeName), fmt.Sprintf("scp -r -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i vagrant.key -P 22 /scripts/manifests/* %s@192.168.66.101:/tmp", sshUser), "copying manifests to the VM")
 		if err != nil {
